7453fb08194282ca131979435b36eda1b6c6462f
Gayatri-k3 <gayatrik1029@gmail.com>
2025-09-11 23:37:48 +0530

    Starting a new project Moviee, using SpringBoot and MongoDB

---
+++ moviee/src/test/java/dev/gayatri/moviee/MovieeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gayatri.moviee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
